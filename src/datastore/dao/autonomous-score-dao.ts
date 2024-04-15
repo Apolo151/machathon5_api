505272e0df5143138a0e7fbf03b8ba1aa725dfbe
Apolo151 <abdallah.ibrahim5115@gmail.com>
2024-04-15 23:33:19 +0200

    update autonomous DAO, more get functions

--- src/datastore/dao/autonomous-score-dao.ts
+++ src/datastore/dao/autonomous-score-dao.ts
@@ -1,7 +1,13 @@
-import { AutonomousSubmission } from "../../types";
+import {
+  AutonomousScore,
+  AutonomousSubmission,
+  CompetitionTeam,
+} from "../../types";
 
 export interface AutonomousSubmissionDao {
-    createSubmission(teamSubmission: AutonomousSubmission): Promise<void>;
-    getTeamSubmissions(teamCode: string): Promise<AutonomousSubmission[]>;
-    getTopScores(): Promise<AutonomousSubmission[]>;
+  createSubmission(teamSubmission: AutonomousSubmission): Promise<void>;
+  getAllSubmissions(): Promise<AutonomousSubmission[]>;
+  getAllTeams(): Promise<CompetitionTeam[]>;
+  getTeamSubmissions(teamCode: string): Promise<AutonomousSubmission[]>;
+  getTopScores(): Promise<AutonomousScore[]>;
 }
