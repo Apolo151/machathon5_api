32e5189f09e276b42e28eeead4a2b7689fff7db4
Apolo151 <abdallah.ibrahim5115@gmail.com>
2024-04-16 05:36:24 +0200

    initial setup for unit tests

--- __tests__/autonomous.test.ts
+++ __tests__/autonomous.test.ts
@@ -1,13 +1,12 @@
 import request from "supertest";
 import { createServer } from "../src/index";
-import { App } from "supertest/types";
 
 require("dotenv").config();
 
 const app = createServer();
 
 describe("Autonomous competition teams", () => {
-  describe("GET /autonomous-race/teams", () => {
+  describe("get all teams registered for the competition", () => {
     it("should return 200 & the list of all teams registered for the competition", async () => {
       return request(await app)
         .get("/autonomous-race/teams")
@@ -18,28 +17,43 @@ describe("Autonomous competition teams", () => {
         });
     });
   });
-});
-describe("GET /autonomous-race/top-scores", () => {
-  it("should return 200 & the list of each team top-score sorted according to the best score", async () => {
-    return request(await app)
-      .get("/autonomous-race/top-scores")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then((res) => {
-        expect(res.statusCode).toBe(200);
-      });
+  describe("register a new team for the competition", () => {
+    it("should add a new team and return 200", async () => {
+      return request(await app)
+        .get("/autonomous-race/teams")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((res) => {
+          expect(res.statusCode).toBe(200);
+        });
+    });
   });
 });
 
-// TODO implement other autonomous competition unit tests
-describe("GET /autonomous-race/submissions", () => {
-  it("should return 200 & the list of all submissions to the competition", async () => {
-    return request(await app)
-      .get("/autonomous-race/submissions")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then((res) => {
-        expect(res.statusCode).toBe(200);
-      });
+describe("Autonomous competition submissions", () => {
+  describe("get all competition submissions", () => {
+    it("should return 200 & the list of all submissions to the competition", async () => {
+      return request(await app)
+        .get("/autonomous-race/submissions")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((res) => {
+          expect(res.statusCode).toBe(200);
+        });
+    });
+  });
+});
+
+describe("Autonomous competition top-scores", () => {
+  describe("get the competition top scores (sorted) for the leaderboard", () => {
+    it("should return 200 & the list of each team top-score sorted according to the best score", async () => {
+      return request(await app)
+        .get("/autonomous-race/top-scores")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((res) => {
+          expect(res.statusCode).toBe(200);
+        });
+    });
   });
 });
--- __tests__/summit.test.ts
+++ __tests__/summit.test.ts
@@ -1,18 +1,24 @@
-import request from 'supertest'
-const app = require("../src/index")
+import request from "supertest";
+import { createServer } from "../src/index";
 
 require("dotenv").config();
 
-describe('GET /summit/attendees', () => {
-    it('should return 200 & the list of all people registered for the summit', async () => {
-        return request(app)
-        .get('/summit/attendees')
-        .expect('Content-Type', /json/)
+const app = createServer();
+
+describe("Summit", () => {
+  describe("get all people registered for the summit", () => {
+    it("should return 200 & the list of all people registered for the summit", async () => {
+      return request(await app)
+        .get("/summit/attendees")
+        .expect("Content-Type", /json/)
         .expect(200)
         .then((res) => {
-            expect(res.statusCode).toBe(200);
-        })
+          expect(res.statusCode).toBe(200);
+        });
     });
+  });
+  describe("check if a person is already registered for the summit (by email)", () => {});
+  describe("register a new person for the summit", () => {});
 });
 
 // TODO implement other summit unit tests
