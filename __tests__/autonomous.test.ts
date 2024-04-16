9da4bbe77e14fa9643d329073710199cb632da13
Apolo151 <abdallah.ibrahim5115@gmail.com>
2024-04-16 04:51:46 +0200

    added autonomous get unit tests

--- __tests__/autonomous.test.ts
+++ __tests__/autonomous.test.ts
@@ -1,19 +1,41 @@
-import request from 'supertest'
-const app = require("../src/index")
+import request from "supertest";
+const app = require("../src/index");
 
 require("dotenv").config();
 
-describe('GET /autonomous-race/top-scores', () => {
-    it('should return 200 & the list of each team top-score sorted according to the best score',
-     async () => {
-        return request(app)
-        .get('/autonomous-race/top-scores')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .then((res) => {
-            expect(res.statusCode).toBe(200);
-        })
-    });
+describe("GET /autonomous-race/top-scores", () => {
+  it("should return 200 & the list of each team top-score sorted according to the best score", async () => {
+    return request(app)
+      .get("/autonomous-race/top-scores")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.statusCode).toBe(200);
+      });
+  });
 });
 
 // TODO implement other autonomous competition unit tests
+describe("GET /autonomous-race/submissions", () => {
+  it("should return 200 & the list of all submissions to the competition", async () => {
+    return request(app)
+      .get("/autonomous-race/submissions")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.statusCode).toBe(200);
+      });
+  });
+});
+
+describe("GET /autonomous-race/teams", () => {
+  it("should return 200 & the list of all teams registered for the competition", async () => {
+    return request(app)
+      .get("/autonomous-race/teams")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.statusCode).toBe(200);
+      });
+  });
+});
